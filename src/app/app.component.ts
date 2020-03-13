fa3e4368ddb776836dfdb95fb8d037677458a8ae
Erik Axelsson <e.axelsson@tutanota.com>
2020-03-13 09:31:19 +0100

    clean up code, add comments and some styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,91 +1,97 @@
-import { Component } from '@angular/core';
-import { DbContactComponent } from './db-contact/db-contact.component';
-import { HttpClient } from '@angular/common/http';
-import { DbservService } from './dbserv.service';
+import { Component } from "@angular/core";
+import { DbContactComponent } from "./db-contact/db-contact.component";
+import { HttpClient } from "@angular/common/http";
+import { DbservService } from "./dbserv.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'u07-angular-movie-app';
+  title = "u07-angular-movie-app";
 
-  
-  userFilm: string = '';
-  userActor: string = '';
-  infoId: string = '';
+  // Variables
+  userFilm: string = "";
+  userActor: string = "";
+  infoId: string = "";
+
+  // Variables for recieved API-objects
   response: any;
   movieResponse: any;
   actorResponse: any;
   infoResponse: any;
   actorInfoResponse: any;
   popularResponse: any;
-  movieApi: string = 'https://api.themoviedb.org/3/search/movie?api_key=';
-  movieApiKey: string = 'f8890476361ecd20500c162292f7b291';
-  movieApiQuery: string = '&query=';
-  infoApi: string = 'https://api.themoviedb.org/3/movie/';
-  infoApiKey: string = '?api_key=' + 'f8890476361ecd20500c162292f7b291';
-  popularApi:string = 'https://api.themoviedb.org/3/trending/all/day?api_key=';
-  actorApi: string = 'https://api.themoviedb.org/3/search/person?api_key=';
-  actorInfoApi: string = 'https://api.themoviedb.org/3/person/';
 
+  // API-query related variables
+  movieApi: string = "https://api.themoviedb.org/3/search/movie?api_key=";
+  movieApiKey: string = "f8890476361ecd20500c162292f7b291";
+  movieApiQuery: string = "&query=";
+  infoApi: string = "https://api.themoviedb.org/3/movie/";
+  infoApiKey: string = "?api_key=" + "f8890476361ecd20500c162292f7b291";
+  popularApi: string = "https://api.themoviedb.org/3/trending/all/day?api_key=";
+  actorApi: string = "https://api.themoviedb.org/3/search/person?api_key=";
+  actorInfoApi: string = "https://api.themoviedb.org/3/person/";
 
-  constructor(private svc: DbservService, private http: HttpClient) {
-   this.svc.printToConsole();
-
-   this.moviePopular();
-
+  constructor(private http: HttpClient) {
+    this.moviePopular();
   }
 
-  ngOnInit() {
-    let movieObs = this.http.get(this.movieApi + this.movieApiKey + this.movieApiQuery + 'seven');
-    movieObs.subscribe((movieResponse) => console.log(movieResponse));
+  ngOnInit() {}
 
-  }
+  // Methods
 
+  /* Search movie by name given by the user.
+   The first line is a call to the API where userFilm is the input from the user.
+   We then get a response with an objects corresponding with the query request.
+   These objects can then be accesed in the html-file by using the movieResponse variable.
+   */
   movieSearch() {
-    this.http.get(this.movieApi + this.movieApiKey + this.movieApiQuery + this.userFilm)
-    .subscribe((response) => {
-      this.movieResponse = response;
-      console.log(this.movieResponse);
-      
-    });
+    this.http
+      .get(
+        this.movieApi + this.movieApiKey + this.movieApiQuery + this.userFilm
+      )
+      .subscribe(response => {
+        this.movieResponse = response;
+      });
   }
 
+  // Search actor or other filmrelated person by name.
   actorSearch() {
-    this.http.get(this.actorApi + this.movieApiKey + this.movieApiQuery + this.userActor)
-    .subscribe((response) => {
-      this.actorResponse = response;
-      console.log(this.actorResponse);
-      
-    });
+    this.http
+      .get(
+        this.actorApi + this.movieApiKey + this.movieApiQuery + this.userActor
+      )
+      .subscribe(response => {
+        this.actorResponse = response;
+      });
   }
 
+  // When you search films by filmname you get a list of matching titles.
+  // When you click on a title this method is used to fetch data from that film.
   movieInfo(idResponse: string) {
-    this.http.get(this.infoApi + idResponse + this.infoApiKey)
-    .subscribe((response) => {
-      this.infoResponse = response;
-    });
-    console.log(idResponse);
+    this.http
+      .get(this.infoApi + idResponse + this.infoApiKey)
+      .subscribe(response => {
+        this.infoResponse = response;
+      });
   }
 
+  // Same logic as the previous method but with actors.
   actorInfo(idResponse: string) {
-    this.http.get(this.actorInfoApi + idResponse + this.infoApiKey)
-    .subscribe((response) => {
-      this.actorInfoResponse = response;
-    });
-    console.log(idResponse);
+    this.http
+      .get(this.actorInfoApi + idResponse + this.infoApiKey)
+      .subscribe(response => {
+        this.actorInfoResponse = response;
+      });
   }
 
+  // This method is called from the constructor when the program start to
+  // get a list of new movies.
   moviePopular() {
-    this.http.get(this.popularApi + this.movieApiKey)
-    .subscribe((response) => {
+    this.http.get(this.popularApi + this.movieApiKey).subscribe(response => {
       this.popularResponse = response;
     });
-    console.log(this.popularResponse);
   }
-
 }
-
-
--- src/app/db-contact/db-contact.component.ts
+++ src/app/db-contact/db-contact.component.ts
@@ -6,29 +6,13 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./db-contact.component.scss']
 })
 export class DbContactComponent implements OnInit {
-
-  user: any;
-  @Input('name') userName: string;
-  isCollapsed: boolean = true;
-  inputText: string = 'testing';
-
-  toggleCollapse() {
-    this.isCollapsed = !this.isCollapsed;
-   
-  }
-
+  
   constructor() { 
-    this.inputText = this.inputText;
+    
   }
 
   ngOnInit(): void {
-    this.user = {
-      name: this.userName,
-      phone: [
-        '123 123',
-        '456 456'
-      ]
-    };
+   
   }
 
 }
